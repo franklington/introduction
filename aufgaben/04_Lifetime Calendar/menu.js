50368b83b8544ea1cc704a2670f843788450743e
fraklington <muehringer@live.com>
2021-12-08 23:02:55 +0100

    A BLOCK A WEEK

---
+++ aufgaben/04_Lifetime Calendar/menu.js
@@ -0,0 +1,163 @@
+
+
+class Checkbox{
+   name; 
+   label;
+   defaultValue;
+   color; 
+   elem;
+   value;
+   count=0;
+   
+   constructor(name, label, defaultValue = true, color,  parent = "#types", count = 0){
+      this.color = color;
+      this.name = name;
+      this.label = label;
+      this.defaultValue = defaultValue;
+      this.value = this.defaultValue;
+      this.parent = parent;
+      this.render();
+      
+   }
+   render = () => {
+
+      XD(this.parent).insertAdjacentHTML("beforeend", this.template());
+      this.elem =  document.getElementById(this.name);
+      this.elem.addEventListener("change", this.change, false);
+   }
+   change = e =>{
+      this.value = e.currentTarget.checked;
+   }
+   bindCheckState = eventHandler =>{
+      this.elem.addEventListener("change", eventHandler, false);
+   }
+
+   template = () =>{
+      return `
+      <style>
+         #${this.name}:checked{background-color:${this.color} !important;}
+         #${this.name}{border-color:${this.color} !important;} 
+      </style>
+      <div class="control">
+           <input type="checkbox"  ${(this.defaultValue ? "checked" : "")} name="${this.name}<" id="${this.name}"><label for="${this.name}<">${this.label} </label>
+      </div>
+      `;
+   }
+   addCount = counter =>{
+      this.count = counter;
+      for (let i = 0; i < this.elem.parentElement.children.length; i++) {
+         if(this.elem.parentElement.children[i].tagName == "LABEL"){
+            this.elem.parentElement.children[i].innerHTML = `${this.label} (${this.count})`;
+         }
+      }
+   }
+
+}
+
+
+let XD = elem => {
+   switch(elem.charAt(0)){
+      case ".":
+         return document.getElementsByClassName(elem.substring(1));
+      case "#":
+         return document.getElementById(elem.substring(1));
+      default:
+         return document.getElementsByTagName(elem);    
+   }
+}
+
+
+
+class SelectBox{
+   name; 
+   label;
+   defaultValue;
+   options; 
+   elem;
+   value;
+   
+   constructor(name, label, options, defaultValue, parent = "#types"){
+      this.options = options;
+      this.name = name;
+      this.label = label;
+      this.defaultValue = defaultValue;
+      this.value = this.defaultValue;
+      this.parent = parent;
+      this.render();
+      
+   }
+   render = () => {
+
+      XD(this.parent).insertAdjacentHTML("beforeend", this.template());
+      this.elem =  document.getElementById(this.name);
+      this.elem.addEventListener("change", this.change, false);
+   }
+   change = e =>{
+      this.value = e.currentTarget.value;
+   }
+   bindChange = eventHandler =>{
+      this.elem.addEventListener("change", eventHandler, false);
+   }
+
+   template = () =>{
+      let template = `
+      <label for="${this.name}">${this.label}</label>
+      <select id="${this.name}" name="${this.name}">
+      `;
+      for(var i = 0; i < this.options.length; i++){
+         let option = this.options[i];
+         if(option == this.defaultValue){
+            template += `<option selected value="${option}"">${option}</option>`;
+         }
+         else{
+            template += `<option value="${option}"">${option}</option>`;
+         }
+       
+      }
+      template += `</select>`;
+      return template;
+   }
+
+}
+
+
+class Input{
+   name; 
+   label;
+   defaultValue;
+   elem;
+   value;
+   
+   constructor(name, label, defaultValue, parent = "#types"){
+      this.name = name;
+      this.label = label;
+      this.defaultValue = defaultValue;
+      this.value = this.defaultValue;
+      this.parent = parent;
+      this.render();
+      
+   }
+   render = () => {
+
+      XD(this.parent).insertAdjacentHTML("beforeend", this.template());
+      this.elem =  document.getElementById(this.name);
+      this.elem.addEventListener("change", this.change, false);
+   }
+   change = e =>{
+      this.value = e.currentTarget.value;
+   }
+   bindChange = eventHandler =>{
+      this.elem.addEventListener("change", eventHandler, false);
+   }
+
+   template = () =>{
+      let template = `
+      <label for="${this.name}">${this.label}</label>
+      <input type="number" id="${this.name}" name="${this.name}"
+       min="10" max="100" value="${this.defaultValue}">
+      `;
+
+      return template;
+   }
+
+}
---
+++ aufgaben/04_Lifetime Calendar/sketch.js
@@ -0,0 +1,107 @@
+const width = 1800;
+const height = 900;
+let data;
+let menu;
+let statistics;
+
+
+
+
+
+
+
+function preload() {
+  //img = loadImage('dnb_land_ocean_ice.2012.3600x1800.jpg');
+  data = loadTable("lifetime.csv", 'ssv', 'header');
+
+}
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  let countries = data.getColumn("Country");
+  let genders = ["female", "divers", "male"];
+  menu ={
+    country: new SelectBox("countries", "country",countries, "Austria"),
+    genders: new SelectBox("genders", "gender",genders, "female"),
+    age: new Input("age", "age", 27),
+
+
+  };
+ 
+  
+
+
+  statistics = new Array();
+  let rows = data.getRowCount();
+  //Setup 3D Points
+  for(var r = 0; r < rows; r++){
+      
+
+    var country = data.getString(r, "Country");
+    var divers  = data.getString(r, "Both sexes");
+    var male  = data.getString(r, "Male");
+    var female = data.getString(r, "Female");
+
+    statistics[country] = { "female": female, "male": male, "divers": divers};
+
+  }
+
+  
+
+  //let lights = new Checkbox("lights", "lights", true, "#ff0000", "#types");
+ 
+}
+
+
+function draw() {
+  background(230);
+  let selectedCountry = menu.country.value;
+  let selectedSex = menu.genders.value;
+  let age = menu.age.value;
+
+  let averageLifespan = statistics[selectedCountry][selectedSex];
+  let boxes = parseInt(averageLifespan * 52.1786);
+  let agedBoxes = parseInt(age * 52.1786);
+  let boxCount = 0;
+  let padding = 50;
+  let size = 12; 
+  let step = size+2;
+
+  stroke(0);
+  fill(237, 24, 70);
+  for(var y = 0+padding; y+padding < windowHeight; y+= step){
+    for(var x = 0+padding; x+padding < windowWidth; x+= step){
+      if(boxCount >= boxes){
+        break;
+      }
+      if(boxCount >= agedBoxes){
+        noFill();
+      }
+      rect(x,y,size,size);
+      boxCount++;
+    }
+    if(boxCount >= boxes){
+      break;
+    }
+  }
+  noStroke();
+  fill(41);
+  textSize(40);
+  text("average age "+ averageLifespan, windowWidth - (padding+400), windowHeight - 100, 400, 400)
+  
+
+  
+
+
+  
+
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+
